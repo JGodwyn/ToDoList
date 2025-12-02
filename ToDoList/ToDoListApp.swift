6b9fccc71a9e02c9a0123463a0057a5233e280ab
Godwin John <jon.godwynn@gmail.com>
2025-12-02 15:29:48 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Gdwn16 on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
